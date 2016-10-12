d413cc7d7d30b0dfec8436859df6efa6cfc80fe3
李瑞勤 <ruiqin.li@eascs.com>
2016-10-12 14:46:58 +0800

    增加了framework target 支持carthage

---
+++ Framework/Framework.h
@@ -0,0 +1,19 @@
+//
+//  Framework.h
+//  Framework
+//
+//  Created by Chapter Zhang on 16/10/12.
+//  Copyright © 2016年 小码哥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Framework.
+FOUNDATION_EXPORT double FrameworkVersionNumber;
+
+//! Project version string for Framework.
+FOUNDATION_EXPORT const unsigned char FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Framework/PublicHeader.h>
+
+#import "MJRefresh.h"
