7a7b1c7efc48d2e97116a0fd82479444693c4d9d
李瑞勤 <ruiqin.li@eascs.com>
2016-11-12 10:36:57 +0800

    修改bundle 拷贝的方式

---
+++ MJRefreshExample/Classes/MJMyView.h
@@ -0,0 +1,16 @@
+//
+//  MJMyView.h
+//  MJRefreshExample
+//
+//  Created by Chapter Zhang on 16/10/26.
+//  Copyright © 2016年 小码哥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MJMyView : UIView
+
+@property (nonatomic) IBInspectable NSInteger test;
+
+
+@end
---
+++ MJRefreshExample/Classes/MJMyView.m
@@ -0,0 +1,21 @@
+//
+//  MJMyView.m
+//  MJRefreshExample
+//
+//  Created by Chapter Zhang on 16/10/26.
+//  Copyright © 2016年 小码哥. All rights reserved.
+//
+
+#import "MJMyView.h"
+
+@implementation MJMyView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
